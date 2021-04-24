ef6537819ec135e49eb2d79d561bd26c851594de
wkchan.linus <wkchan.linus@gmail.com>
2021-04-24 18:42:10 +1200

    Draw a diagram with D3 from CSV

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		'jest/globals': true
-	},
-	extends: ['prettier', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-		sourceType: 'module',
-		tsconfigRootDir: __dirname
-	},
-	plugins: ['jest', '@typescript-eslint'],
-	ignorePatterns: ['bin/*', '.eslintrc.js', 'test-config/*', 'workbox-config.js', 'webpack.config.js'],
-	rules: {
-		'react/display-name': 'off',
-		'@typescript-eslint/no-explicit-any': 'off'
-	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	}
+    env: {
+        browser: true,
+        node: true,
+        'jest/globals': true
+    },
+    extends: ['prettier', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        tsconfigRootDir: __dirname
+    },
+    plugins: ['jest', '@typescript-eslint'],
+    ignorePatterns: ['bin/*', '.eslintrc.js', 'test-config/*', 'workbox-config.js', 'webpack.config.js'],
+    rules: {
+        'react/display-name': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        "@typescript-eslint/ban-ts-comment": "off"
+
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
 };
